df827e0c5bdc71493ccf84b3b1fa6edf00198c08
alexshchegretsov <nydollz77@gmail.com>
2019-03-26 14:07:33 +0300

    Check string done

--- src/check_string.py
+++ src/check_string.py
@@ -4,25 +4,17 @@ letters = [0] * 26
 length = len(letters)
 
 for char in string.lower():
-    if char >= 'a' and char <= 'z':
+    if 'a' <= char <= 'z':
         char_number = ord(char) - 97
         letters[char_number] += 1
-print(letters)
 
-
+max_char_repeat = max(letters)
 while i < length:
-    item = letters[i]
-    if item > 1:
-        current_char = chr(item + 97)
-        string_repeats = current_char * letters[item]
-        print(string_repeats)
-        if len(string_repeats) == max(letters):
-            print(item)
-            print(string_repeats)
+    if letters[i] > 0:
+        char = chr(i + 97)
+        char_repeat = char * letters[i]
+        char_repeat_length = len(char_repeat)
+        if char_repeat_length == max_char_repeat:
+            print(char)
             break
     i += 1
-
-# for char in range(26):
-#     string_repeats = chr(char + 97) * letters[char]
-#     if len(string_repeats) == max(letters):
-#         print(chr(char + 97))
