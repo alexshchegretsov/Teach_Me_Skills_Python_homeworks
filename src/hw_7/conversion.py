f0b65bb7443c9c1b5bc40ff0370a8de6aa4beb75
alexshchegretsov <nydollz77@gmail.com>
2019-04-05 00:30:42 +0300

    Modyfied conversion and add cw_8 2nd try

--- src/hw_7/conversion.py
+++ src/hw_7/conversion.py
@@ -21,41 +21,41 @@ def km_to_miles(km: Union[int, float]) -> float:
     return round(km / 1.609, 2)
 
 
-def pounds_to_kilos(n: Union[int, float]) -> float:
+def pounds_to_kilos(pounds: Union[int, float]) -> float:
     """Returns pounds as a representation in kilograms"""
-    return round(n * 0.453592, 2)
+    return round(pounds * 0.453592, 2)
 
 
-def kilos_to_pounds(n: Union[int, float]) -> float:
+def kilos_to_pounds(kilos: Union[int, float]) -> float:
     """Returns kilograms as a representation in English pounds"""
-    return round(n / 0.453592, 2)
+    return round(kilos / 0.453592, 2)
 
 
-def ounces_to_grams(n: Union[int, float]) -> float:
+def ounces_to_grams(ounces: Union[int, float]) -> float:
     """Returns ounces as a representation in grams"""
-    return round(n * 28.3495, 3)
+    return round(ounces * 28.3495, 3)
 
 
-def grams_to_ounces(n: Union[int, float]) -> float:
+def grams_to_ounces(grams: Union[int, float]) -> float:
     """Returns grams as a representation in troy ounces"""
-    return n / 28.3495
+    return grams / 28.3495
 
 
-def gallons_to_liters(n: Union[int, float]) -> float:
+def gallons_to_liters(gallons: Union[int, float]) -> float:
     """Returns gallons as a representation in liters"""
-    return round(n * 3.78541, 2)
+    return round(gallons * 3.78541, 2)
 
 
-def liters_to_gallons(n: Union[int, float]) -> float:
+def liters_to_gallons(liters: Union[int, float]) -> float:
     """Returns liters as a representation in American gallons"""
-    return round(n / 3.8541, 2)
+    return round(liters / 3.8541, 2)
 
 
-def pints_to_liters(n: Union[int, float]) -> float:
+def pints_to_liters(pints: Union[int, float]) -> float:
     """Returns pints as a representation in liters"""
-    return round(n * 0.658261, 2)
+    return round(pints * 0.658261, 2)
 
 
-def liters_to_pints(n: Union[int, float]) -> float:
+def liters_to_pints(liters: Union[int, float]) -> float:
     """Returns liters as a representation in English pints"""
-    return round(n / 0.658261, 2)
+    return round(liters / 0.658261, 2)
